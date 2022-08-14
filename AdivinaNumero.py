81c1ea35669e9777e75b2c3ba2e694dbae302145
roilht <roilhi@gmail.com>
2022-08-13 20:18:11 -0700

    initial commit

---
+++ AdivinaNumero.py
@@ -0,0 +1,29 @@
+"""
+Juego de adivinar el número con el usuario
+"""
+
+import random
+
+def main():
+    """
+    Damos como entrada los límites superior e inferior de un intervalo numérico.
+    El usuario deberá adivinar el número aleatorio seleccionado de ese intervalo.
+    """
+    lim_in = int(input("Ingrese el valor más pequeño del intervalo: "))
+    lim_sup = int(input("Ingresa el valor más grande del intervalo: " ))
+    num_secreto = random.randint(lim_in, lim_sup)
+    intentos = 0
+    while True:
+        intentos += 1
+        num_usuario = int(input("Trata de adivinar el número secreto ingresando un valor numérico "))
+        if num_usuario < num_secreto:
+            print('Intenta con un número más grande')
+        elif num_usuario > num_secreto:
+            print('Intenta con un número más pequeño')
+        else:
+            print(f'¡Ganaste, adivinaste el número secreto en {intentos} intentos')
+            break
+if __name__ == "__main__":
+    main()
+
+
